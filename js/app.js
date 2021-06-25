1adc4058fd67674febf81ba5e93c5602db30ea0b
ignaciocervino <ignaciocervino@gmail.com>
2021-06-25 01:31:15 -0300

    #05 Validating the forms

--- js/app.js
+++ js/app.js
@@ -1,16 +1,34 @@
-//Variables
-
+/** VARIABLES */
 const btnEnviar = document.querySelector('#enviar');
-
+//Variables para campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
 eventListeners();
 function eventListeners(){
+    //Para arrancar la app
     document.addEventListener('DOMContentLoaded',iniciarApp);
+    //Campos del formulario
+    email.addEventListener('blur',validarFormulario);
+    asunto.addEventListener('blur',validarFormulario);
+    mensaje.addEventListener('blur',validarFormulario);
 }
 
 
-//Funciones
+/** FUNCIONES */
 function iniciarApp(){
-    btnEnviar.disable=true;
+   btnEnviar.disable=true;
     //1° Deshabilito el boton de enviar
     btnEnviar.classList.add('cursor-not-allowed','opacity-50');
+}
+
+//Valida formulario
+function validarFormulario(e){
+
+    if(e.target.value.length>0){
+        console.log('Si hay algo');
+    }
+    else{
+        e.target.classList.add('border','border-red-500');
+    }
 }
